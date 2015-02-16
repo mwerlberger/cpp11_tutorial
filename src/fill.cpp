6a2917120114346f28348a7630cdd3c33dbfd0d5
Manuel Werlberger <code@werlberger.org>
2015-02-16 23:22:53 +0100

    added array demo

--- src/fill.cpp
+++ src/fill.cpp
@@ -4,6 +4,7 @@
 #include <iomanip>
 #include <algorithm>
 #include <functional>
+#include <array>
 
 #include "helpers.hpp"
 
@@ -12,11 +13,12 @@ int main(int /*argc*/, char** /*argv*/)
 {
   try
   {
-    int length = 10;
+    const int length = 10;
 
     std::uint8_t pixels_8u[length];
     std::uint16_t pixels_16u[length];
 
+    //--------------------------------------------------------------------------
     memset(pixels_8u,  1, length*sizeof(std::uint8_t));
     memset(pixels_16u, 1, length*sizeof(std::uint16_t));
 
@@ -33,6 +35,7 @@ int main(int /*argc*/, char** /*argv*/)
     }
     std::cout << std::endl << std::endl;
 
+    //--------------------------------------------------------------------------
     // use std::fill
     std::fill(pixels_8u, pixels_8u+length, 1);
     std::fill(pixels_16u, pixels_16u+length, 1);
@@ -48,8 +51,18 @@ int main(int /*argc*/, char** /*argv*/)
     {
       std::cout << static_cast<std::int32_t>(px) << " ";
     }
-    std::cout << std::endl;
+    std::cout << std::endl << std::endl;
 
+    //--------------------------------------------------------------------------
+    // you know about array?
+    std::array<std::uint8_t, length> arr;
+    std::fill(arr.begin(), arr.end(), 128);
+    std::cout << "array:" << std::endl;
+    for (const auto& v : arr)
+    {
+      std::cout << static_cast<int>(v) << ' ';
+    }
+    std::cout << std::endl << std::endl;
 
   }
     catch (std::exception& e)
@@ -57,8 +70,6 @@ int main(int /*argc*/, char** /*argv*/)
     std::cout << "[exception] " << e.what() << std::endl;
     assert(false);
   }
-
-
   return EXIT_SUCCESS;
 }
 
